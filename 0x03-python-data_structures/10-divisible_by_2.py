b06cc952e9f2def862f0a26f1c4c4341681831f5
davehh1211 <davehh1211@gmail.com>
2020-05-06 03:00:42 +0000

    multilpes of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,8 +4,8 @@ def divisible_by_2(my_list=[]):
         return
     new_list = my_list.copy()
     for i in range(0, len(my_list)):
-        if i % 2 == 0:
+        if i % 2 > 0:
             new_list[i] = True
         else:
-            new_list[i] = False      
+            new_list[i] = False     
     return new_list
