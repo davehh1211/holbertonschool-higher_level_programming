20887edc669bd151b84cca4968d48fb90a49792a
davehh1211 <davehh1211@gmail.com>
2020-05-07 22:54:28 +0000

    multiply keys

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    n_dictionary = a_dictionary.copy()
+    for key in n_dictionary:
+        n_dictionary[key] = n_dictionary[key] * 2
+        return n_dictionary
