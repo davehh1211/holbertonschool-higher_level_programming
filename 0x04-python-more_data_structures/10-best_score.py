636e2bf8479581e2f2ab91f9d6d2a93d52b0336a
davehh1211 <davehh1211@gmail.com>
2020-05-07 23:50:57 +0000

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,6 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        n_list = sorted(a_dictionary)
-        return n_list[-1]
+        #n_list = sorted(a_dictionary.key())
+        n_list = max(a_dictionary, key=a_dictionary.get)
+        return n_list
