751439e87fcccd31b4eaa9e051b2d0bdf0923e7d
davehh1211 <davehh1211@gmail.com>
2020-05-08 02:15:53 +0000

    map in a map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    matrix2 = matrix.copy()
-    return [list(map((lambda x: x * x), row)) for row in matrix2]
+    return list(map((lambda row: (list(map((lambda a: a * a), row)))), matrix))
