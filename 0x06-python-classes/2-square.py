81de833309d2a61b02a4884c18bec83e58bf9422
davehh1211 <davehh1211@gmail.com>
2020-05-19 20:31:10 +0000

    raise errors and mistakes

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Class that defines a squared
+    If the size is not an integer raises a Type Error
+    If the size is less than zero raises a Value error
+    Args:
+        Size (int): Private, Size of the square
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
