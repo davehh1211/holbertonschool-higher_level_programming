b19bb742f2896da9473bd4a5d09f5cbc3fdb5e4f
davehh1211 <davehh1211@gmail.com>
2020-05-19 21:39:35 +0000

    fix pep8 requirement

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -18,7 +18,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-            
+
     def area(self):
         "definition of area of the square"
         area = self.__size * self.__size
