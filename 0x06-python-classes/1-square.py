bf02b16a55c7d0d3ddba95dff1b72b8c39900bdf
davehh1211 <davehh1211@gmail.com>
2020-05-19 21:31:21 +0000

    fix comments on shebang

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"1-square.py - defines a square with a private attribute"
+
+
 class Square:
     """
-    Class that defines a squared
-    
+    Class that defines a square
     Args:
-        Size (int): Size of the square
+        Size (int): Private, Size of the square
     """
     def __init__(self, size):
+        "Initialization of instance with size"
         self.__size = size
