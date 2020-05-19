ab2b887ef45e312c42eca4f38ddb57e732a5659c
davehh1211 <davehh1211@gmail.com>
2020-05-19 21:15:12 +0000

    fix comments

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Square:
-    """ Class that only defines a Square.
-    
+    """ 
+    Class that only defines a Square.
     Attributes: No attributes
     Methods: Pass
     """
