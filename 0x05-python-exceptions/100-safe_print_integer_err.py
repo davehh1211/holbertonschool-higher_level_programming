9a9483c5158d28b008dd7c885a2e48bb214909b3
davehh1211 <davehh1211@gmail.com>
2020-05-19 01:43:52 +0000

    exception added

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        print("Exeption: Unknown format code\
+ 'd' for object of type 'str'", file=sys.stderr)
+        return False
+    print()
