2d68a68fa32901707bd2fe1d049dec052a22810a
davehh1211 <davehh1211@gmail.com>
2020-05-20 02:29:20 +0000

    square exercises conditional v7

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,8 +54,7 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for i in range(0, self.__position[1]):
+        for i in range(self.__position[1]):
             print()
-        for i in range(0, self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
+        for i in range(self.__size):
+            print((" " * self.__position[0]) + ("#" * self.__size))
