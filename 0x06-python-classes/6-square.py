bd9530cbe8bbca433a2d8c6e9bead64e508ff33c
davehh1211 <davehh1211@gmail.com>
2020-05-20 02:10:51 +0000

    square exercises conditional

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,10 +38,10 @@ class Square:
     @position.setter
     def position(self, value):
         "proteting the data from wrong values"
-        if (type(value) is not tuple or value[0] < 0 or value[1] < 0 or
-                type(value[0]) is not int or type(value[1]) is not int):
+        if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif len(value) is not 2:
+        elif (type(value) != tuple or value[0] < 0 or value[1] < 0 or
+                type(value[0]) != int or type(value[1]) != int):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
