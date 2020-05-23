7c46ab4ec66125b9c8cd5e5082c6c9be424603d8
davehh1211 <davehh1211@gmail.com>
2020-05-23 05:14:47 +0000

    fix addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -19,8 +19,13 @@ def add_integer(a, b=98):
     Returns:
         int -- result of the addition of two.
     """
+
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    elif type(b) not in (int, float):
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
+    if type(a) is float('inf') or type(b) is float('inf'):
+        raise OverflowError("cannot convert float infinity to integer")
+    if type(a) is float('Nan') or type(b) is float('NaN'):
+        raise OverflowError("cannot convert float infinity to integer")
     return int(a) + int(b)
