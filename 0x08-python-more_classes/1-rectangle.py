cf2099b4f6449ac10f64855f384babbe236854ac
davehh1211 <davehh1211@gmail.com>
2020-05-25 23:12:20 +0000

    first definition of rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""this function defines a rectangle area.
+"""
+
+
+class Rectangle:
+    """ Class that defines the a rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        """Instance to initialize the variables.
+        Keyword Arguments:
+            width {int} -- [the longitude of the figure] (default: {0})
+            height {int} -- [the altitude of the figure] (default: {0})
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.height
+
+    @height.setter
+    def height(self, altitude):
+        if type(altitude) is not int:
+            raise TypeError("height must be an integer")
+        if altitude < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = altitude
