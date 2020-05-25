470bc029360d4a65915766a790f11e988d599654
davehh1211 <davehh1211@gmail.com>
2020-05-25 23:13:00 +0000

    zero exercise for rectangles

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Funtion for rectagles
+    """
+
+
+class Rectangle:
+    """function that defines a rectangle.
+    Attributes:
+        No attributes
+    Methods:
+        Pass
+    """
+    pass
