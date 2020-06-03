e5bf00703db5ab459c6a6696c68b6f718ad81438
davehh1211 <davehh1211@gmail.com>
2020-06-03 01:34:44 +0000

    class subclass v1

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Write a class Rectangle that inherits from BaseGeometry
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Write a class Rectangle that inherits from BaseGeometry
+
+    Arguments:
+        BaseGeometry {[class]} -- it has the area and valuevaldiator methods
+    """
+
+    def __init__(self, width, height):
+        """isntance of value validator
+        Arguments:
+            width {[int]} -- [longitude]
+            height {[int]} -- [altitude]
+        """
+        self.integer_validator(self, width)
+        self.integer_validator(self, height)
+        self.__width = width
+        self.__height = height
