e40245a17e558d356830924dbd9a6644e2090c5d
davehh1211 <davehh1211@gmail.com>
2020-06-03 00:44:13 +0000

    exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry
+(based on 5-base_geometry.py)
+Raises:
+    Exception: [area() is not implemented]
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry
+    """
+
+    def area(self):
+        """Area in exception
+
+        Raises:
+            Exception: [area() is not implemented]
+                """
+        raise Exception("area() is not implemented")
