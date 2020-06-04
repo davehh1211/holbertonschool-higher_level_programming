062c57d98fb2876c4c421d65ad43e3d1da03811e
davehh1211 <davehh1211@gmail.com>
2020-06-04 01:07:26 +0000

    script

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+import os
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+if __name__ == '__main__':
+    """main
+    """
+    filename = "add_item.json"
+    if os.path.isfile(filename):
+        "evaluate if the filename exist"
+        listin = load_from_json_file(filename)
+        "loads the list file if it exists"
+        listin += sys.argv[1:]
+        "it concatenates the list with the new arguemnts"
+        save_to_json_file(list(listin), filename)
+        "it saves the arguments"
+    else:
+        "if the file does not exist, it creates it."
+        save_to_json_file(sys.argv[1:], filename)
