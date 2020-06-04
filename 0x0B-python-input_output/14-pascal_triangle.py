7fff77b60f3c252a6160a4ccb013056d43b0f27c
davehh1211 <davehh1211@gmail.com>
2020-06-04 04:26:53 +0000

    test2

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -12,20 +12,20 @@ def pascal_triangle(n):
     if n <= 0:
         return
 
-    lista = [[1],[1,1]]
+    lista = [[1], [1, 1]]
 
     # bucle que se generara tantas veces como lineas vayamos a tener
-    for i in range(1,n):
+    for i in range(1, n):
 
         # inicializamos la linea
         linea = [1]
 
         # bucle por cada uno de los valores de la anterior linea
-        for j in range(0,len(lista[i])-1):
+        for j in range(0, len(lista[i]) - 1):
 
             # añadimos a la lista los nuevos valores
             # sumamos el valor de la lista anterior con el siguinte
-            linea.extend([ lista[i][j] + lista[i][j+1] ])
+            linea.extend([lista[i][j] + lista[i][j+1]])
 
         # añadimos el ultimo valor a la nueva linea
         # siempre es un 1 igual que el primero
@@ -34,5 +34,5 @@ def pascal_triangle(n):
         # añadimos la linea a la lista
         lista.append(linea)
 
-    #devolvemos la lista ya creada
+    # devolvemos la lista ya creada
     return lista
