04626354e5fde24aaf5470a435a9f89a25e25c90
davehh1211 <davehh1211@gmail.com>
2020-06-09 18:21:28 +0000

    fix the square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,9 +15,6 @@ class Square(Rectangle):
     """
 
     def __init__(self, size, x=0, y=0, id=None):
-        #self.size = size
-        #self.__width = size
-        #self.__height = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
