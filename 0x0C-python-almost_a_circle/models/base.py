28fd13b3313a0f7969ef25989648b2c73ec8a4a3
davehh1211 <davehh1211@gmail.com>
2020-06-09 17:11:03 +0000

    update version to check

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@ id attribute in all your future
 classes and to avoid duplicating
 the same code (by extension, same bugs)
 """
+import json
+import csv
 
 
 class Base:
@@ -23,3 +25,82 @@ class Base:
         else:
             "it an argument is given, it passes normally"
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """[summary]
+
+                Args:
+                        list_dictionaries ([type]): [description]
+
+                Returns:
+                        [type]: [description]
+                """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """[summary]
+
+                Args:
+                        list_objs ([type]): [description]
+                """
+        filename = "{}.json".format(cls.__name__)
+        with open(filename, mode='w', encoding='UTF8') as myfile:
+            if list_objs is None:
+                json.dumps("[]", myfile)
+            else:
+                list_own = []
+                for obj_lists in list_objs:
+                    tmp_dictio = cls.to_dictionary(obj_lists)
+                    list_own.append(tmp_dictio)
+            json_list_dict = cls.to_json_string(list_own)
+            myfile.write(json_list_dict)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """[summary]
+
+        Args:
+            json_string ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(4, 5)
+        elif cls.__name__ == "Square":
+            dummy = cls(5)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """[summary]
+
+                Returns:
+                        [type]: [description]
+                """
+        filename = "{}.json".format(cls.__name__)
+        if not filename:
+            return "[]"
+        else:
+            with open(filename, mode='r', encoding='UTF8') as myfile:
+                list_inst = myfile.read()
+                str_js = cls.from_json_string(list_inst)
+                l_js = []
+                for instan_el in str_js:
+                    aux = cls.create(**instan_el)
+                    l_js.append(aux)
+                return l_js
