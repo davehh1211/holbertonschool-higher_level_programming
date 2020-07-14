3d7efdec280888f1717066ed2f8266b78f8d6c31
davehh1211 <davehh1211@gmail.com>
2020-07-14 20:19:51 +0000

    remove constraint query

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,9 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL, 
-	CONSTRAINT cities_ibfk_1
-	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
-	ON RESTRIC CASCADE,
+	state_id INT NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
 	name VARCHAR(256) NOT NULL
 );
