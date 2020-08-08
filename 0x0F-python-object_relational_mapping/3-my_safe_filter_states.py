d621a8ccf1fdfd6cf597f3b26f054f224376d4f7
davehh1211 <davehh1211@gmail.com>
2020-08-08 18:18:17 +0000

    task 3.0

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""[summary]
+    """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         charset="utf8")
+    cur = db.cursor()
+    # HERE I have to know SQL to grab all states in my database
+    cur.execute(
+        "SELECT * FROM states WHERE BINARY name = %s;", (sys.argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
