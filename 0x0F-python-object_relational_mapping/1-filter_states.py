c0cbfe31aa4c76cbc9cafdddc999e6d0709a2c1c
davehh1211 <davehh1211@gmail.com>
2020-08-08 17:45:23 +0000

    task 1.1 ADDED binary regexp

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
                          charset="utf8")
     cur = db.cursor()
     # HERE I have to know SQL to grab all states in my database
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states WHERE name REGEXP BINARY '^N' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
