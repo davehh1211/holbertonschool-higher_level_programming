97cd347cd00554f58c8ac9730d860d6b2e861d56
davehh1211 <davehh1211@gmail.com>
2020-08-08 17:52:10 +0000

    task 2.2 fix query and pep8

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,6 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
-                         port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3],
@@ -15,7 +14,9 @@ if __name__ == "__main__":
     cur = db.cursor()
     # HERE I have to know SQL to grab all states in my database
     cur.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(sys.argv[4]))
+        "SELECT * FROM states \
+        WHERE BINARY name = '{}' \
+        ORDER BY states.id ASC".format(sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
