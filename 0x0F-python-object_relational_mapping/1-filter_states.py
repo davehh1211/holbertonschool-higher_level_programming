5025e99924b51047f99f8bc903fe06e0540ebaf5
davehh1211 <davehh1211@gmail.com>
2020-08-08 17:40:53 +0000

    task 1.0

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""[summary]
+    """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         charset="utf8")
+    cur = db.cursor()
+    # HERE I have to know SQL to grab all states in my database
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
