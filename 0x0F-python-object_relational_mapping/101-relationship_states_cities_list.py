13290cd36de3f39c34586fdd2d6ba6e9d5f4a7f6
davehh1211 <davehh1211@gmail.com>
2020-08-09 23:15:44 +0000

    task 16.0

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""script that prints the first State object from the database
+hbtn_0e_6_usa"""
+from sqlalchemy import (create_engine)
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    """Connecting"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    file1 = session.query(State).join(
+        City).order_by(State.id, City.id).order_by(State.id, City.id).all()
+    for i in file1:
+        print("{}: {}".format(i.id, i.name))
+        for j in i.cities:
+            print("\t{}: {}".format(j.id, j.name))
+
+    session.close()
+
+
+# result = session.query(Customer).join(Invoice).filter(Invoice.amount == 8500)
+# for row in result:
+#    for inv in row.invoices:
+#       print (row.id, row.name, inv.invno, inv.amount)
