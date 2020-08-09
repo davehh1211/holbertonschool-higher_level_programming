7d1e286ac6f8ea2f6b2cd5134ef287b475a6641e
davehh1211 <davehh1211@gmail.com>
2020-08-09 21:46:05 +0000

    relation state

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""[summary]
+    """
+from sqlalchemy import Integer, String, Column
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """[summary]
+
+    Args:
+    Base ([type]): [description]
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+
+    name = Column(String(128), nullable=False)
+    cities = relationship('City')
+    #state = relationship('State')
