27b9035b87e37108dec06963ebbb459fc16d8a1a
davehh1211 <davehh1211@gmail.com>
2020-08-09 23:32:32 +0000

    task 17.0

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that prints the first State object from the database
+hbtn_0e_6_usa"""
+from sqlalchemy import (create_engine)
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    """Connecting"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    file1 = session.query(State).all()
+    for i in file1:
+        for j in i.cities:
+            print("{}: {} -> {}".format(j.id, j.name, i.name))
+
+    session.close()
