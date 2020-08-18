e3e3ebfc0cd14bfa1a40c0e5cbcabae65f927f07
davehh1211 <davehh1211@gmail.com>
2020-08-18 22:52:32 +0000

    task 6.1

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-"""	"""
+"""modulo peak
+"""
 
 
 def find_peak(list_of_integers):
+	"""Write a function that finds a peak in a list of unsorted integers.
+
+	Args:
+		list_of_integers ([type]): [description]
+
+	Returns:
+		[type]: [description]
+	"""
     if list_of_integers:
         list_of_integers.sort()
         return list_of_integers[-1]
