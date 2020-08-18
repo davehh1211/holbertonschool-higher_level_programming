c5a85ff27be10a03abf6f914bad86218317d9f3a
davehh1211 <davehh1211@gmail.com>
2020-08-18 21:32:13 +0000

    task 3.1 add different option to read substring

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$2" | grep Allow | awk '{print $2, $3, $4}'
+curl -sI "$2" | grep Allow | awk '{print substr($0, index($0,$2))}'
