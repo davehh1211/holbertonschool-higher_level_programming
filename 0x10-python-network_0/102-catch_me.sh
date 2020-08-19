d8610f0200f372f2c9226e8756cd03518d2d1b72
davehh1211 <davehh1211@gmail.com>
2020-08-19 01:13:18 +0000

    task 9.0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -si -X PUT -d "user_id=98" -H "origin: HolbertonSchool" -L 0.0.0.0:5000/catch_me "$1"
+curl -si -X PUT -d "user_id=98" -H "origin: HolbertonSchool" -L 0.0.0.0:5000/catch_me
