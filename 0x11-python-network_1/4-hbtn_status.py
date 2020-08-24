2fbe870638f613bd418bb3de9776516ee4ef0b56
davehh1211 <davehh1211@gmail.com>
2020-08-24 22:52:31 +0000

    task 4.0

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches
+https://intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html.decode('utf8')))
