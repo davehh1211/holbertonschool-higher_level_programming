a75e1f1616d7abcec33260fdc1ad67b966986327
davehh1211 <davehh1211@gmail.com>
2020-08-25 01:24:27 +0000

    task 8.0

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        dictjson = r.json()
+        id_dictio = dictjson.get('id')
+        name = dictjson.get('name')
+        if len(dictjson) > 0 or id_dictio or name:
+            print("[{}] {}".format(id_dictio, name))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
