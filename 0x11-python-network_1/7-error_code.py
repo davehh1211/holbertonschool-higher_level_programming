6d107c79308413ca45027d878d6b411003099213
davehh1211 <davehh1211@gmail.com>
2020-08-25 00:08:22 +0000

    task 7.0

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-	"""[summary]
-	"""
+""" Write a Python script that takes
+in a URL, sends a request to the URL
+and displays the body of the response.
+"""
 import requests
 import sys
 
