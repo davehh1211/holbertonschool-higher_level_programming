185e6b01dd6ed693805efdd6bf0b35f1b33dc3c6
davehh1211 <davehh1211@gmail.com>
2020-09-15 22:58:03 +0000

    task 7.1 fix condition

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,8 +3,8 @@ let myNumber = process.argv[2];
 if (isNaN(myNumber)) {
   console.log('Missing number of occurrences');
 } else {
-  while (myNumber != 0) {
+  while (myNumber > 0) {
     console.log('C is fun');
-	myNumber--;
+    myNumber--;
   }
 }
