f3306a8aa1349589b718c9e5ab0082d14387d3bb
davehh1211 <davehh1211@gmail.com>
2020-09-16 03:50:57 +0000

    task 11.6

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,7 @@
 let maxNum = 0;
 const arg = process.argv.slice(2);
 if (arg.length > 1) {
-  arg.sort((a,b)=>a-b);
-  console.log(arg)
+  arg.sort((a, b) => a - b);
   maxNum = arg[arg.length - 2];
 }
-console.log(Number(maxNum));
+console.log(maxNum);
